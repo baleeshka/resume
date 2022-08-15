0720fc44a2a0842b519277641a51faf243fec593
baleeshka <110674536+baleeshka@users.noreply.github.com>
2022-08-15 15:50:14 +0300

    Add files via upload

---
+++ htmlService.js
@@ -0,0 +1,48 @@
+function elipsis(string = '', maxLength = 30) {
+    if(string.length > maxLength) {
+        return string.substring(0, maxLength) + '...'
+    }
+    return string
+}
+
+class HTMLService {
+    paintProduct(product) {
+        return `
+            <li data-id="${product.id}">
+            <img src="${product.image}" title="${product.title}" /> 
+            <small>${elipsis(product.title,15)}</small>
+            <small><strong>$${product.price}</strong></small>
+            </li>
+        `
+    }
+
+    paintProducts(products = []) {
+        return products.map(this.paintProduct).join('')
+    }
+
+    paintCartItem(item) {
+        return `
+          <li data-type="remove" data-id="${item.id}">
+            (${item.amount}) 
+            ${item.title}
+            <strong>$${item.price}</strong>
+          </li>
+        `
+      }
+
+    paintCart({items, totalPrice}) {
+        if(items.length === 0) {
+            return `<p>Корзина пуста</p>`
+        }
+        return `
+        <ul class="cart-list">
+          ${items.map(this.paintCartItem).join('')}
+        </ul>
+        <hr />
+        <p class="info">
+          <span>Общая цена: <strong>$${totalPrice.toFixed(2)}</strong></span>
+          <button class="clear" data-type="clear">Очистить</button>
+        </p>
+      `
+    }
+}
